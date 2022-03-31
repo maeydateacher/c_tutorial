e9fdbcf4ad059a4c09529eb04645c905dd023505
maeydateacher <102676938+maeydateacher@users.noreply.github.com>
2022-03-31 18:54:39 +0500

    Add files via upload

---
+++ tutorial_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a = 10 ; 
+    int b = 5 ;
+    
+    int c = a + b ;
+    printf("%d\n",c);
+    
+    int d = a - b ;
+    printf("%d\n",d);
+    
+    int e = a * b ;
+    printf("%d\n",e);
+    
+    int f = a / b;
+    printf("%d\n",f);
+    
+    return 0;
+}
