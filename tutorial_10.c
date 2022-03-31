3a57da4eefe2ed5db63b0a4cd97d8f612a785aae
maeydateacher <102676938+maeydateacher@users.noreply.github.com>
2022-03-31 20:17:08 +0500

    Add files via upload

---
+++ tutorial_10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main( ) {
+
+   int a;
+   int b;
+   
+   printf( "Enter a value :");
+   scanf( "%d", &a);
+
+   printf( "\n a:  %d ", a);
+   
+   printf("Enter b value :");
+   scanf("%d",&b);
+   
+   printf("\n b : %d", b);
+
+   return 0;
+}
