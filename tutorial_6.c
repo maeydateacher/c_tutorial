5a5beaf7cae7c769615a08ab33c6323f8327a73d
maeydateacher <102676938+maeydateacher@users.noreply.github.com>
2022-03-31 20:02:24 +0500

    Add files via upload

---
+++ tutorial_6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a = 5 ;
+    switch(a){
+        case 1 :
+        printf("a is 1");
+        break;
+        case 2 :
+        printf("a is 2");
+        break;
+        case 3:
+        printf("a is 3");
+        break;
+        case 4:
+        printf("a is 4");
+        break;
+        case 5:
+        printf("a is 5");
+        break;
+        default:
+        printf("a is greater or smaller than 5");
+        break;
+    }
+    
+    return 0;
+}
